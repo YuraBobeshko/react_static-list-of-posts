8a479cc05d7b6671260459436f36c4cc10104bd1
YuraBobeshko <gorobech55@gmail.com>
2019-10-30 11:37:27 +0200

    Updates

---
+++ precache-manifest.8edef904174e34713a1f8feb959aade7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc909564faf1ee9a299170c2eceffd93",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "b91c501dcf035835c027",
+    "url": "/react_static-list-of-posts/static/css/main.cae03474.chunk.css"
+  },
+  {
+    "revision": "bec6397d68ef6b7a19b5",
+    "url": "/react_static-list-of-posts/static/js/2.03b73e7a.chunk.js"
+  },
+  {
+    "revision": "b91c501dcf035835c027",
+    "url": "/react_static-list-of-posts/static/js/main.d7b20e42.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
